2705e555d1c1c71dc80d87dd7a8e633359d5db29
barnabyrobson <test@test-accounts-macbook-pro.local>
2013-01-31 22:55:15 -0800

    pyflakes: no "import *"

--- mpython.py
+++ mpython.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 
-from ast import *
+import ast
 import sys
 
 def name(x, ctx):
-  return Name(id = x, ctx = ctx, lineno = 0, col_offset = 0)
+  return ast.Name(id = x, ctx = ctx, lineno = 0, col_offset = 0)
 
 def call(f, *args):
-  f = name(f, Load())
-  return Call(func = f, args = list(args), lineno = 0,
+  f = name(f, ast.Load())
+  return ast.Call(func = f, args = list(args), lineno = 0,
               col_offset = 0, keywords = [], vararg = None)
 
 def func(args, body):
-  return Lambda(args = arguments(args = args, defaults = []),
+  return ast.Lambda(args = ast.arguments(args = args, defaults = []),
                 body = body, vararg = None, lineno = 0, col_offset = 0)
 
-unit = Tuple(elts = [], lineno=0, col_offset = 0, ctx = Load())
+unit = ast.Tuple(elts = [], lineno=0, col_offset = 0, ctx = ast.Load())
 
 def transform(elt, generators):
   elt = call("__singleton__", elt)
@@ -23,17 +23,17 @@ def transform(elt, generators):
   for generator in generators[-1::-1]:
     for i in generator.ifs[-1::-1]:
       elt = call("__concatMap__",
-                 func([name('_', Param())], elt),
-                 IfExp(i,
+                 func([name('_', ast.Param())], elt),
+                 ast.IfExp(i,
                     call('__singleton__', unit),
                     call('__fail__'), lineno=0, col_offset=0))
 
     elt = call("__concatMap__",
-               func([name(generator.target.id, Param())], elt),
+               func([name(generator.target.id, ast.Param())], elt),
                generator.iter)
   return elt
 
-class RewriteComp(NodeTransformer):
+class RewriteComp(ast.NodeTransformer):
   def visit_ListComp(self, node):
     return transform(RewriteComp().visit(node.elt),
                      [RewriteComp().visit(generator) for generator in node.generators])
@@ -48,7 +48,7 @@ def __fail__():
   return []
 
 source = open(sys.argv[1]).read()
-e = compile(source, "<string>", "exec", PyCF_ONLY_AST)
+e = compile(source, "<string>", "exec", ast.PyCF_ONLY_AST)
 e = RewriteComp().visit(e)
 f = compile(e, sys.argv[1], "exec")
 print f
